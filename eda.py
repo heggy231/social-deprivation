1404901100f6e76061d11957773223e92c60a180
dingocuster <gibson.daniel.d@gmail.com>
2015-08-04 17:13:32 -0700

    update

--- eda.py
+++ eda.py
@@ -5,7 +5,7 @@ Spyder Editor
 This temporary script file is located here:
 /home/d/.spyder2/.temp.py
 """
-
+import os
 import pandas as pd
 import numpy as np
 #from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
@@ -22,24 +22,34 @@ Import metadata and files
 Note: 'pd.read_table' doesn't work so well
 '''
 
-meta = pd.read_excel('/data/DeprivedAuthorsTextAnalysis.xls')
+meta = pd.read_excel('/data/modifiedDeprivedAuthorsTextAnalysis.xls')
 #binarize deprivation
 
 meta['deprivation']= meta['Deprivation? (Y/N)'].apply(lambda x: x=='Y')
 meta.drop(u'Deprivation? (Y/N)',axis=1)
-#make dummy variables for Type of deprivation and Genre
+#make dummy variables for Type of deprivation and Genre, and author?
+meta=meta.join(pd.get_dummies(meta['Type of Deprivation']))
+
+#get years?
+
 
 #open txts
 f= 'data/allTextData/Blanqui-Textes_Choisi-1971-Nonfiction-N.txt'
 with open(f,'r') as ex:
     text = ex.read()
-    
+
+files = []
+texts = pd.DataFrame()
+for f in os.listdir("/mydir"):
+    if f.endswith(".txt"):
+        files.append(f)
+        meta
 #open docx
 d=docx.opendocx('data/allTextData/Brown-Letter_dated_November_16-1859-Y.docx')
 dtext = docx.getdocumenttext(d)
 
-#using ex
-
+#using existing features
+correlation = meta.corr().deprivation
 #tf-ifd
 
 #parts of speech
